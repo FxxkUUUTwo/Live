70da8ea2fe87b8c9557c74048c7c574d04c1d56a
wanghaixin <414348801@qq.com>
2016-11-29 15:04:08 +0800

    xiugai

--- app/src/main/java/com/other/project/live/adapters/MainRecyclerViewAdapter.java
+++ app/src/main/java/com/other/project/live/adapters/MainRecyclerViewAdapter.java
@@ -1,33 +1,58 @@
 package com.other.project.live.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.other.project.live.model.MainModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by wanghaixin on 16/11/29.
  */
-public class MainRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class MainRecyclerViewAdapter extends RecyclerView.Adapter<MainRecyclerViewAdapter.ViewHolder> {
+
+    private List<MainModel> data;
+
+    private LayoutInflater inflater;
+
+    public MainRecyclerViewAdapter(List<MainModel> data, Context context) {
+
+        if (data != null) {
+            this.data = data;
+        } else {
+            this.data = new ArrayList<>();
+        }
+        inflater = LayoutInflater.from(context);
+    }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+
         return null;
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, int position) {
 
     }
+
 
     @Override
     public int getItemCount() {
 
 
-        return 0;
+        return data != null ? data.size() : 0;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-
+private LinearLayout hot_service;
         public ViewHolder(View itemView) {
             super(itemView);
         }
